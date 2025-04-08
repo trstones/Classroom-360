4ef40d788a59b15e148098eeea59f7e5e3339cc3
trstones <trstones@colby.edu>
2025-04-08 16:04:03 -0400

    Update inject-snippet.js

--- inject-snippet.js
+++ inject-snippet.js
@@ -1,5 +1,5 @@
 window.addEventListener("load", function () {
-    console.log("Simple script loaded successfully!");
+    console.log("Simple script loaded successfully! Step 2.");
     const newDiv = document.createElement("div"); // Create a new div
     newDiv.innerHTML = "<h2>Welcome to the page!</h2><p>This content was added dynamically using JavaScript.</p>";
     document.body.appendChild(newDiv); // Append the new div to the body
