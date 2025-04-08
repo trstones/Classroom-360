7145969f9081788f925d3ef61e144aa456f0cf31
trstones <trstones@colby.edu>
2025-04-08 16:16:27 -0400

    Update inject-snippet.js

--- inject-snippet.js
+++ inject-snippet.js
@@ -1,9 +1,9 @@
 window.addEventListener("load", function () {
-    console.log("Simple script loaded successfully! Step 3.");
+    console.log("Simple script loaded successfully! Step 4.");
 
     const existingDiv = document.getElementById("external-snippet");
     if (existingDiv) {
-        existingDiv.innerHTML = "<h2>Content Added!</h2><p>This is dynamically added content.</p>";
+        existingDiv.innerText = "<h2>Content Added!</h2><p>This is dynamically added content.</p>";
     } else {
         console.error("Element not found.");
     }
