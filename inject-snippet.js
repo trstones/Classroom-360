68ee8435243938441456e5a9c58d16327e05ed86
trstones <trstones@colby.edu>
2025-04-08 14:37:43 -0400

    Update inject-snippet.js

--- inject-snippet.js
+++ inject-snippet.js
@@ -1,17 +1,18 @@
-(function waitForContainer() {
-  const container = document.getElementById("external-snippet");
-
-  if (!container) {
-    setTimeout(waitForContainer, 100);
-    return;
+document.addEventListener("DOMContentLoaded", function () {
+  function loadSnippet() {
+    var container = document.getElementById("external-snippet");
+    if (container) {
+      fetch("https://trstones.github.io/Classroom-360/snippet.html")
+        .then(response => response.text())
+        .then(html => {
+          container.innerHTML = html;
+        })
+        .catch(err => {
+          console.error("Error loading snippet:", err);
+        });
+    } else {
+      setTimeout(loadSnippet, 100);
+    }
   }
-
-  fetch(https://trstones.github.io/Classroom-360/snippet.html")
-    .then(response => response.text())
-    .then(html => {
-      container.innerHTML = html;
-    })
-    .catch(err => {
-      console.error("Error loading snippet:", err);
-    });
-})();
+  loadSnippet();
+});
