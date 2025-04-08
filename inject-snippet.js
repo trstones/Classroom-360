8f5809ef1b5f25a0edcf529f875760b1b668e708
trstones <trstones@colby.edu>
2025-04-08 16:00:35 -0400

    Update inject-snippet.js

--- inject-snippet.js
+++ inject-snippet.js
@@ -1,4 +1,3 @@
 window.addEventListener("load", function () {
-    const content = "<div><h2>Dynamic Content</h2><p>This is added via an external script.</p></div>";
-    document.body.insertAdjacentHTML('beforeend', content);
+    console.log("Simple script loaded successfully!");
 });
