a2e40265077d126aabc80c0a39ab3fc181b2177b
trstones <trstones@colby.edu>
2025-08-22 15:42:10 -0400

    Update inject-snippet.js

--- inject-snippet.js
+++ inject-snippet.js
@@ -46,8 +46,7 @@ window.addEventListener("load", function () {
             let html = "";
             html += '<center><a class="btn btn-default btn-block" style="width:75%" href="https://colby.teamdynamix.com/TDClient/1928/Portal/Requests/ServiceDet?ID=55250" role="button">Return to Classroom Catalog</a></center>'
             if (dict["Is Venue"] == "Yes") {
-                html += '<br>';
-                html += '<center><a class="btn btn-default btn-block" style="width:75%" href="https://colby.teamdynamix.com/TDClient/1928/Portal/Requests/ServiceDet?ID=55467" role="button">Return to Venue Lookup</a></center>'
+                html += '<center><a class="btn btn-default btn-block" style="width:75%; margin:10px 0;" href="https://colby.teamdynamix.com/TDClient/1928/Portal/Requests/ServiceDet?ID=55467" role="button">Return to Venue Lookup</a></center>'
             }
             html += '<p><h3>General Information</h3></p>';
             html += '<ul>';
